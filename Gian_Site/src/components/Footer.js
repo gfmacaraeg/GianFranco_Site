36e07651b057f4cdbd5b1f0c0a1abf1e3311a5aa
gfmacaraeg <gianfranco.macaraeg@my.tccd.edu>
2020-02-04 11:42:54 -0600

    Site Upload

---
+++ Gian_Site/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer" >
+                <div className="inner" style={{opacity: 1.0}}>
+                    <ul className="icons">
+                        <li><a href="https://github.com/gfmacaraeg" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/gfmacaraeg/" className="icon fa-linkedin"><span className="label">Dribbble</span></a></li>
+                        <li><a href="mailto: gfmacaraeg@gmail.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                    </ul>
+                    <ul className="copyright">
+                        <li>&copy; GianFranco</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer
