779516761378364a72efd47a6da29744332c23a4
gfmacaraeg <gianfranco.macaraeg@my.tccd.edu>
2020-02-06 23:49:02 -0600

    Added project links

--- Gian_Site/src/pages/index.js
+++ Gian_Site/src/pages/index.js
@@ -21,8 +21,8 @@ import full06 from '../assets/images/fulls/06.jpg'
 
 const DEFAULT_IMAGES = [
     { id: '1', source: full01, thumbnail: thumb01, caption: 'Travel Pal', description: 'A CRUD web application that allows users to list their upcoming travel plans and other registered users can join trips based on all pre-existing trips. '},
-    { id: '2', source: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '3', source: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '2', source: full02, thumbnail: thumb02, caption: 'Security Cam', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '3', source: full03, thumbnail: thumb03, caption: 'Personal Portfolio Website', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     { id: '4', source: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     { id: '5', source: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     { id: '6', source: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
