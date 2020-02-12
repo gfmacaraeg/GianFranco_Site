bed07f517d56f4b0962efc8cec34ddd78e3cc681
gfmacaraeg <gianfranco.macaraeg@my.tccd.edu>
2020-02-12 12:26:24 -0600

    cleaning stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,9 @@ class Header extends React.Component {
                     <a href="#" className="image avatar"><img src={avatar} alt="My picture" /></a>
                     {/* <h1 style={{color:"#98eab9"}}><strong>GianFranco Macaraeg</strong><br /> */}
                     
-                    <h1 style={{color:"#49bf9d"}}><strong>GianFranco Macaraeg</strong><br />
-                    FULL STACK SOFTWARE DEVELOPER<br />
-                    </h1>
+                    <h1><strong>GianFranco Macaraeg</strong></h1>
+                    <h1 style={{color:"#49bf9d"}}>FULL STACK SOFTWARE DEVELOPER</h1>
+                    
                 </div>
                 <Footer />
             </header>
